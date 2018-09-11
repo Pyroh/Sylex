b715744bc3544388c954cb95a5deec03de54b4bc
Pierre TACCHI <pyroh@icloud.com>
2018-09-11 14:42:03 +0200

    [IMP] rowmovements

--- Sources/Sylex/ArrayDiff/structs/DifferenceSet.swift
+++ Sources/Sylex/ArrayDiff/structs/DifferenceSet.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct DifferenceSet {
     public let removedIndices: [Int]
     public let insertedIndices: [Int]
-    public let movedIndices: [SwappedIndicesPair]
+    public let movedIndexPair: [SwappedIndexPair]
     
     public var removedIndexSet: IndexSet {
         return IndexSet(self.removedIndices)
@@ -18,27 +18,33 @@ public struct DifferenceSet {
     public var insertedIndexSet: IndexSet {
         return IndexSet(self.insertedIndices)
     }
+    public var localMovedIndexPairSet: Set<SwappedIndexPair.IndexPair> {
+        return self.movedIndexPair.lazy.map { $0.local }.set()
+    }
+    public var globalMovedIndexPairSet: Set<SwappedIndexPair.IndexPair> {
+        return self.movedIndexPair.lazy.map { $0.global }.set()
+    }
     
     internal init(from results: [DiffResult]) {
-        let output = results.reduce(into: (removed: [Int](), inserted: [Int](), moved: [SwappedIndicesPair]())) { (output, result) in
+        let output = results.reduce(into: (removed: [Int](), inserted: [Int](), moved: [SwappedIndexPair]())) { (output, result) in
             switch result {
             case .removed(let i):
                 output.removed.append(i)
             case .inserted(let i):
                 output.inserted.append(i)
             case let .moved(g, l):
-                output.moved.append(SwappedIndicesPair(g: g, l: l))
+                output.moved.append(SwappedIndexPair(g: g, l: l))
             }
         }
         
         self.removedIndices = output.removed
         self.insertedIndices = output.inserted
-        self.movedIndices = output.moved
+        self.movedIndexPair = output.moved
     }
     
-    internal init(removedIndices: [Int] = [], insertedIndices: [Int] = [], movedIndices: [SwappedIndicesPair] = []) {
+    internal init(removedIndices: [Int] = [], insertedIndices: [Int] = [], movedIndices: [SwappedIndexPair] = []) {
         self.removedIndices = removedIndices
         self.insertedIndices = insertedIndices
-        self.movedIndices = movedIndices
+        self.movedIndexPair = movedIndices
     }
 }
