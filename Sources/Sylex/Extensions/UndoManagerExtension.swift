9268be44d870a5414a0c5678731bd19372f50bab
Pierre TACCHI <pyroh@icloud.com>
2019-03-08 14:57:14 +0100

    [ADD] convenient method for undo registering

---
+++ Sources/Sylex/Extensions/UndoManagerExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UndoManagerExtension.swift
+//  Sylex
+//
+
+import Foundation
+
+@available(OSX 10.11, *)
+public extension UndoManager {
+    public func registerUndo<TargetType: AnyObject, ValueType>(for key: ReferenceWritableKeyPath<TargetType, ValueType>, of target: TargetType, with actionName: String? = nil) {
+        let value: ValueType = target[keyPath: key]
+        if let name = actionName { self.setActionName(name) }
+        self.registerUndo(withTarget: target) { $0[keyPath: key] = value }
+    }
+}
