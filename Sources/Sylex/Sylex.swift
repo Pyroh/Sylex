479bdc36316da5412ffcc3a673e4f134bc6dff25
Pierre TACCHI <pyroh@icloud.com>
2019-07-26 15:51:52 +0200

    add .? operator

--- Sources/Sylex/Sylex.swift
+++ Sources/Sylex/Sylex.swift
@@ -20,16 +20,22 @@ infix operator **: ExponentationPrecedence
 infix operator <?>
 
 infix operator ..: MapOperatorPrecedence
+infix operator .?: MapOperatorPrecedence
 
-public func ..<T, R>(lhs: T, rhs: (T) throws -> (R)) rethrows -> R { return try rhs(lhs) }
-public func ..<T, R>(lhs: T?, rhs: (T?) throws -> (R)) rethrows -> R { return try rhs(lhs) }
-public func ..<T, R>(lhs: T, rhs: (T) throws -> (R?)) rethrows -> R? { return try rhs(lhs) }
-public func ..<T, R>(lhs: T?, rhs: (T?) throws -> (R?)) rethrows -> R? { return try rhs(lhs) }
+public typealias F<T, R> = (T) throws -> (R)
+public typealias A<T, R> = (T?) throws -> (R)
+public typealias M<T, R> = (T) throws -> (R?)
+public typealias P<T, R> = (T?) throws -> (R?)
 
-public func ..<T, R>(lhs: T, rhs: ((T) throws -> (R))?) rethrows -> R? { return try rhs?(lhs) }
-public func ..<T, R>(lhs: T?, rhs: ((T?) throws -> (R))?) rethrows -> R? { return try rhs?(lhs) }
-public func ..<T, R>(lhs: T, rhs: ((T) throws -> (R?))?) rethrows -> R? { return try rhs?(lhs) }
-public func ..<T, R>(lhs: T?, rhs: ((T?) throws -> (R?))?) rethrows -> R? { return try rhs?(lhs) }
+public func ..<T, R>(lhs: T, rhs: F<T, R>) rethrows -> R  { return try rhs(lhs) }
+public func ..<T, R>(lhs: T?, rhs: A<T, R>) rethrows -> R { return try rhs(lhs) }
+public func ..<T, R>(lhs: T, rhs: M<T, R>) rethrows -> R? { return try rhs(lhs) }
+public func ..<T, R>(lhs: T?, rhs: P<T, R>) rethrows -> R? { return try rhs(lhs) }
+
+public func .?<T, R>(lhs: T, rhs: F<T, R>?) rethrows -> R? { return try rhs?(lhs) }
+public func .?<T, R>(lhs: T?, rhs: A<T, R>?) rethrows -> R? { return try rhs?(lhs) }
+public func .?<T, R>(lhs: T, rhs: M<T, R>?) rethrows -> R? { return try rhs?(lhs) }
+public func .?<T, R>(lhs: T?, rhs: P<T, R>?) rethrows -> R? { return try rhs?(lhs) }
 
 public func clamp<T: Comparable>(_ x: T, _ min: T, _ max: T) -> T {
     return Swift.min(Swift.max(x, min), max)
