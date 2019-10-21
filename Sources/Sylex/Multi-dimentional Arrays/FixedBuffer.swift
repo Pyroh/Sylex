0ae81fc863b4b815845a3612aa4e7991d9e52f7f
Pierre TACCHI <pyroh@icloud.com>
2019-10-21 17:47:30 +0200

    [ADD] FixedDataTable and storage
    ⚠️ tests needed ⚠️

--- Sources/Sylex/Multi-dimentional Arrays/FixedBuffer.swift
+++ Sources/Sylex/Multi-dimentional Arrays/FixedBuffer.swift
@@ -1,12 +1,11 @@
 //
 //  FixedBuffer.swift
-//  
+//  Sylex
 //
-//  Created by Pierre TACCHI on 11/10/2019.
+//  Created by Pierre TACCHI on 14/10/2019.
 //
 
-import Foundation
-
+// MARK: - Main
 final class FixedBuffer<Element>: ManagedBuffer<Int, Element> {
     class func create(withCapacity cap: Int) -> FixedBuffer {
         FixedBuffer.create(minimumCapacity: cap, makingHeaderWith: { _ in cap }) as! FixedBuffer
@@ -27,12 +26,6 @@ final class FixedBuffer<Element>: ManagedBuffer<Int, Element> {
         self.withUnsafeMutablePointers { (header, elements) -> () in
             header.deinitialize(count: 1)
             elements.deinitialize(count: self.capacity)
-        }
-    }
-    
-    func contiguousArray(fromIndex i: Int, count: Int) -> [Element] {
-        self.withUnsafeMutablePointerToElements { (elements) -> [Element] in
-            [Element](UnsafeBufferPointer(start: elements + i, count: count))
         }
     }
     
@@ -58,6 +51,120 @@ final class FixedBuffer<Element>: ManagedBuffer<Int, Element> {
     }
 }
 
+// MARK: - Array interaction
+extension FixedBuffer {
+    func toArray() -> [Element] {
+        self.withUnsafeMutablePointerToHeader { self.contiguousArray(fromIndex: 0, count: $0.pointee) }
+    }
+    
+    func contiguousArray(fromIndex i: Int, count: Int) -> [Element] {
+        self.withUnsafeMutablePointerToElements { (elements) -> [Element] in
+            [Element](UnsafeBufferPointer(start: elements + i, count: count))
+        }
+    }
+}
+
+// MARK: - Mutations
+extension FixedBuffer {
+    func moveElements(fromRange srcRange: Range<Int>, intoCorrespondingRangeStatingAt dstIndex: Int, fillingGapWith value: Element) {
+        
+        let srcIndex = srcRange.lowerBound
+        let count = srcRange.count
+        let capacity = self.withUnsafeMutablePointerToHeader { $0.pointee }
+        
+        assert(srcIndex >= 0 && srcIndex < capacity, "Source pointer location is out of range.")
+        assert(dstIndex >= 0 && dstIndex < capacity, "Destination pointer location is out of range.")
+        assert(srcIndex + count <= capacity, "Source range is out of range.")
+        assert(dstIndex + count <= capacity, "Destination range is out of range.")
+        
+        let srcRange = srcIndex..<srcIndex + count
+        let dstRange = dstIndex..<dstIndex + count
+        
+        if dstRange.overlaps(srcRange) {
+            self.withUnsafeMutablePointerToElements {
+                let tmp = UnsafeMutablePointer<Element>.allocate(capacity: count)
+                tmp.initialize(from: $0 + srcIndex, count: count)
+                ($0 + dstIndex).moveAssign(from: tmp, count: count)
+                tmp.deallocate()
+            }
+        } else {
+            self.withUnsafeMutablePointerToElements { ($0 + dstIndex).moveAssign(from: ($0 + srcIndex), count: count) }
+        }
+        
+        let emptyied = Set(srcRange).subtracting(Set(dstRange))
+        
+        if emptyied.count > 0, let startIndex = emptyied.min() {
+            self.initialize(range: .init(startIndex: startIndex, count: emptyied.count), with: value)
+        }
+    }
+    
+    func swapElement(at i: Int, withElementAt j: Int) {
+        let capacity = self.withUnsafeMutablePointerToHeader { $0.pointee }
+        
+        assert(i >= 0 && i < capacity, "Index \(i) is out of bounds.")
+        assert(j >= 0 && j < capacity, "Index \(j) is out of bounds.")
+        
+        guard i != j else { return }
+        
+        (self[i], self[j]) = (self[j], self[i])
+    }
+    
+    func swapElements(in srcRange: Range<Int>, withElementsInCorrespondingRangeStatingAt dstIndex: Int) {
+        let capacity = self.withUnsafeMutablePointerToHeader { $0.pointee }
+        let allowedRange = 0..<capacity
+        
+        guard srcRange.count > 1 else {
+            self.swapElement(at: srcRange.lowerBound, withElementAt: dstIndex)
+            return
+        }
+        let dstRange = dstIndex..<dstIndex + srcRange.count
+        assert(srcRange.lowerBound >= allowedRange.lowerBound && srcRange.upperBound <= allowedRange.upperBound, "Source range out of range.")
+        assert(dstRange.lowerBound >= allowedRange.lowerBound && dstRange.upperBound <= allowedRange.upperBound, "Destination range out of range.")
+        
+        guard srcRange != dstRange else { return }
+        let count = srcRange.count
+        
+        self.withUnsafeMutablePointerToElements {
+            let srcTmp = UnsafeMutablePointer<Element>.allocate(capacity: count)
+            let dstTmp = UnsafeMutablePointer<Element>.allocate(capacity: count)
+            
+            srcTmp.initialize(from: $0 + srcRange.lowerBound, count: count)
+            dstTmp.initialize(from: $0 + dstRange.lowerBound, count: count)
+            
+            ($0 + dstRange.lowerBound).moveAssign(from: srcTmp, count: count)
+            ($0 + srcRange.lowerBound).moveAssign(from: dstTmp, count: count)
+            
+            srcTmp.deallocate()
+            dstTmp.deallocate()
+        }
+    }
+    
+    func copyElements(from srcArray: [Element], at dstIndex: Int) {
+        let capacity = self.withUnsafeMutablePointerToHeader { $0.pointee }
+        let endCopyIndex = dstIndex + srcArray.count
+        
+        assert(endCopyIndex <= capacity, "Not enough room to copy the given elements starting at index \(dstIndex).")
+        
+        self.withUnsafeMutablePointerToElements { (elements) in
+            srcArray.withUnsafeBufferPointer { (srcElements) in
+                (elements + dstIndex).assign(from: srcElements.baseAddress!, count: srcArray.count)
+            }
+        }
+    }
+    
+    private func initialize(range: Range<Int>, with value: Element) {
+        self.withUnsafeMutablePointers { (header, elements) in
+            let capacity = header.pointee
+            
+            assert(range.lowerBound >= 0 && range.lowerBound < capacity, "Start index out of range.")
+            assert(range.upperBound <= capacity, "Initializing range is out of range")
+            
+            (elements + range.lowerBound).initialize(repeating: value, count: range.count)
+        }
+    }
+}
+
+// MARK: - Zeroable specifics
 extension FixedBuffer where Element: Zeroable {
     class func create(withCapacity cap: Int) -> FixedBuffer {
         let buffer = FixedBuffer.create(minimumCapacity: cap, makingHeaderWith: { _ in cap }) as! FixedBuffer
@@ -67,8 +174,17 @@ extension FixedBuffer where Element: Zeroable {
         
         return buffer
     }
+    
+    func moveElement(from srcIndex: Int, to dstIndex: Int) {
+        self.moveElements(fromRange: .init(startIndex: srcIndex, count: 1), intoCorrespondingRangeStatingAt: dstIndex, fillingGapWith: .zero)
+    }
+    
+    func moveElements(fromRange srcRange: Range<Int>, intoCorrespondingRangeStartingAt dstIndex: Int) {
+        self.moveElements(fromRange: srcRange, intoCorrespondingRangeStatingAt: dstIndex, fillingGapWith: .zero)
+    }
 }
 
+// MARK: - Optionals specifics
 extension FixedBuffer where Element: ExpressibleByNilLiteral {
     class func create(withCapacity cap: Int) -> FixedBuffer {
         let buffer = FixedBuffer.create(minimumCapacity: cap, makingHeaderWith: { _ in cap }) as! FixedBuffer
@@ -77,5 +193,13 @@ extension FixedBuffer where Element: ExpressibleByNilLiteral {
         }
         
         return buffer
+    }
+    
+    func moveElement(from srcIndex: Int, to dstIndex: Int) {
+        self.moveElements(fromRange: .init(startIndex: srcIndex, count: 1), intoCorrespondingRangeStatingAt: dstIndex, fillingGapWith: nil)
+    }
+    
+    func moveElements(fromRange srcRange: Range<Int>, intoCorrespondingRangeStartingAt dstIndex: Int) {
+        self.moveElements(fromRange: srcRange, intoCorrespondingRangeStatingAt: dstIndex, fillingGapWith: nil)
     }
 }
--- Sources/Sylex/Multi-dimentional Arrays/FixedDataTable.swift
+++ Sources/Sylex/Multi-dimentional Arrays/FixedDataTable.swift
@@ -5,10 +5,12 @@
 //  Created by Pierre TACCHI on 14/10/2019.
 //
 
-import Foundation
-
 public struct FixedDataTable<Element> {
-    struct FixedDataTableColumnIterator<Element>: IteratorProtocol {
+    public enum Dimension {
+        case row(Int)
+        case col(Int)
+    }
+    private struct FixedDataTableColumnIterator<Element>: IteratorProtocol {
         typealias Buffer = FixedBuffer<Element>
         
         private unowned var buffer: Buffer
@@ -39,14 +41,6 @@ public struct FixedDataTable<Element> {
     public let rowCount: Int
     public let count: Int
     
-    public var rows:[[Element]] {
-        (0..<self.rowCount).lazy.map { self.linearIndex(forRow: $0, column: 0) }.map { self.storage.contiguousArray(fromIndex: $0, count: self.columnCount) }
-    }
-    
-    public var columns: [[Element]] {
-        (0..<self.columnCount).lazy.map { index in AnySequence<Element>.init({ FixedDataTableColumnIterator(atIndex: index, withBuffer: self.storage, columnCount: self.columnCount) }) }.map([Element].init)
-    }
-    
     public init(rows: Int, columns: Int) {
         assert(columns > 0, "Coloumn count must be greater than or equal to 1.")
         assert(rows > 0, "Row count must be greater than or equal to 1.")
@@ -64,7 +58,8 @@ public struct FixedDataTable<Element> {
         assert(columnsCount.count == 1, "Each subarray must hold the same number of items.")
         assert(columnsCount.first! > 0, "Each subarray must hold at least one element.")
         let rows = array.count
-        let buffer = array.reduce(into: [Element]()) { (acc, element) in acc.append(contentsOf: element) }
+        
+        let buffer = [Element](array.joined())
         
         self = .init(rows: rows, columns: columnsCount.first!, buffer: buffer)
     }
@@ -85,17 +80,24 @@ public struct FixedDataTable<Element> {
         get {
             assert(row < self.rowCount, "Row index out of bounds.")
             assert(col < self.rowCount, "Column index out of bounds.")
-            return self.storage[self.linearIndex(forRow: row, column: col)]
+            return self.storage[self.linearizedIndex(forRow: row, column: col)]
         }
         mutating set {
             assert(row < self.rowCount, "Row index out of bounds.")
             assert(col < self.rowCount, "Column index out of bounds.")
-            self.storage[self.linearIndex(forRow: row, column: col)] = newValue
+            self.storage[self.linearizedIndex(forRow: row, column: col)] = newValue
         }
     }
     
-    private func linearIndex(forRow row: Int, column: Int) -> Int {
+    private func linearizedIndex(forRow row: Int, column: Int) -> Int {
         row * self.columnCount + column
+    }
+    
+    private func delinearizedIndex(_ index: Int) -> (row: Int, column: Int) {
+        let row = index / self.columnCount
+        let column = index % self.columnCount
+
+        return (row, column)
     }
     
     private mutating func ensureUniqueness() {
@@ -105,6 +107,7 @@ public struct FixedDataTable<Element> {
     }
 }
 
+// MARK: - Zeroable specific intializer
 public extension FixedDataTable where Element: Zeroable {
     init(rows: Int, columns: Int) {
         assert(columns > 0, "Coloumn count must be greater than or equal to 1.")
@@ -118,6 +121,7 @@ public extension FixedDataTable where Element: Zeroable {
     }
 }
 
+// MARK: - ExpressibleByNilLiteral specific intializer
 public extension FixedDataTable where Element: ExpressibleByNilLiteral {
     init(rows: Int, columns: Int) {
         assert(columns > 0, "Coloumn count must be greater than or equal to 1.")
@@ -131,6 +135,63 @@ public extension FixedDataTable where Element: ExpressibleByNilLiteral {
     }
 }
 
+// MARK: - Elements boundaries
+public extension FixedDataTable where Element: Zeroable&Comparable {
+    func min() -> Element {
+        self.rows.compactMap { $0.min() }.min() ?? .zero
+    }
+    
+    func max() -> Element {
+        self.rows.compactMap { $0.max() }.max() ?? .zero
+    }
+}
+
+// MARK: - Access rows and columns
+public extension FixedDataTable {
+    var rows:[[Element]] {
+        (0..<self.rowCount).lazy.map { self.linearizedIndex(forRow: $0, column: 0) }.map { self.storage.contiguousArray(fromIndex: $0, count: self.columnCount) }
+    }
+    
+    var columns: [[Element]] {
+        (0..<self.columnCount).lazy.map { index in AnySequence<Element>({ FixedDataTableColumnIterator(atIndex: index, withBuffer: self.storage, columnCount: self.columnCount) }) }.map([Element].init)
+    }
+    
+    subscript(dimension: Dimension) -> [Element] {
+        get {
+            switch dimension {
+            case .row(let row):
+                assert(row < self.rowCount, "Row index \(row) out of range.")
+                return self.row(at: row)
+            case .col(let col):
+                assert(col < self.columnCount, "Row index \(col) out of range.")
+                return self.column(at: col)
+            }
+        }
+        
+        mutating set {
+            switch dimension {
+            case .row(let row):
+                assert(row < self.rowCount, "Row index \(row) out of range.")
+                self.ensureUniqueness()
+                self.setRow(newValue, at: row)
+            case .col(let col):
+                assert(col < self.columnCount, "Row index \(col) out of range.")
+                self.ensureUniqueness()
+                self.setColumn(newValue, at: col)
+            }
+        }
+    }
+    
+    private func column(at index: Int) -> [Element] {
+        [Element](AnySequence<Element>({ FixedDataTableColumnIterator(atIndex: index, withBuffer: self.storage, columnCount: self.columnCount) }))
+    }
+    
+    private func row(at index: Int) -> [Element] {
+        self.storage.contiguousArray(fromIndex: self.linearizedIndex(forRow: index, column: 0), count: self.columnCount)
+    }
+}
+
+// MARK: - Monadics
 public extension FixedDataTable {
     private var sequenceStorage: AnySequence<Element> {
         AnySequence<Element>({ () -> AnyIterator<Element> in
@@ -146,6 +207,153 @@ public extension FixedDataTable {
     }
     
     func forEach(_ body: (Element) throws -> Void) rethrows {
-        try self.forEach(body)
+        try self.sequenceStorage.forEach(body)
+    }
+    
+    func enumerate(_ body: ((row: Int, column: Int), Element) throws -> Void) rethrows {
+        try self.sequenceStorage.enumerated().forEach { (offset: Int, element: Element) in
+            try body(self.delinearizedIndex(offset), element)
+        }
+    }
+}
+
+// MARK: - Mutate rows and columns
+private extension FixedDataTable {
+    private func setRow(_ array: [Element], at index: Int) {
+        if array.count > self.columnCount {
+            self.storage.copyElements(from: [Element](array[0..<self.columnCount]), at: self.linearizedIndex(forRow: index, column: 0))
+        } else {
+            self.storage.copyElements(from: array, at: self.linearizedIndex(forRow: index, column: 0))
+        }
+    }
+    
+    private func setColumn(_ array: [Element], at index: Int) {
+        guard array.count > 0 else { return }
+        let workArray = array.count <= self.rowCount ? array : [Element](array[0..<self.columnCount])
+        workArray.lazy.enumerated().forEach { (offset, element) in
+            self.storage[self.linearizedIndex(forRow: offset, column: index)] = element
+        }
+    }
+}
+
+// MARK: - Mutations: Zeroable specifics
+public extension FixedDataTable where Element: Zeroable {
+    mutating func moveRow(from srcIndex: Int, to dstIndex: Int) {
+        assert(srcIndex >= 0 && srcIndex < self.rowCount, "Source index out of range.")
+        assert(dstIndex >= 0 && dstIndex < self.rowCount, "Destination index out of range.")
+        self.ensureUniqueness()
+        guard srcIndex != dstIndex else { return }
+        
+        self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: srcIndex, column: 0), count: self.columnCount), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: dstIndex, column: 0))
+    }
+    
+    mutating func moveRows(from srcRange: Range<Int>, to dstIndex: Int) {
+        assert(srcRange.lowerBound >= 0 && srcRange.lowerBound < self.rowCount, "Source range out of range.")
+        assert(srcRange.count + dstIndex <= self.rowCount, "Move not possible as it would overflow.")
+        self.ensureUniqueness()
+        guard srcRange.lowerBound != dstIndex else { return }
+        
+        let moveCount = srcRange.count * self.columnCount
+        self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: srcRange.lowerBound, column: 0), count: moveCount), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: dstIndex, column: 0))
+    }
+    
+    mutating func moveColumn(from srcIndex: Int, to dstIndex: Int) {
+        assert(srcIndex >= 0 && srcIndex < self.columnCount, "Source index out of range.")
+        assert(dstIndex >= 0 && dstIndex < self.columnCount, "Destination index out of range.")
+        self.ensureUniqueness()
+        guard srcIndex != dstIndex else { return }
+        
+        (0..<self.rowCount).forEach {
+            self.storage.moveElement(from: self.linearizedIndex(forRow: $0, column: srcIndex), to: self.linearizedIndex(forRow: $0, column: dstIndex))
+        }
+    }
+    
+    mutating func moveColumns(from srcRange: Range<Int>, to dstIndex: Int) {
+        assert(srcRange.lowerBound >= 0 && srcRange.lowerBound < self.columnCount, "Source range out of range.")
+        assert(srcRange.count + dstIndex <= self.columnCount, "Move not possible as it would overflow.")
+        self.ensureUniqueness()
+        guard srcRange.lowerBound != dstIndex else { return }
+        
+        (0..<self.columnCount).forEach {
+            self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: $0, column: srcRange.lowerBound), count: srcRange.count), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: $0, column: dstIndex))
+        }
+    }
+}
+
+// MARK: - Mutations: ExpressibleByNilLiteral specifics
+public extension FixedDataTable where Element: ExpressibleByNilLiteral {
+    mutating func moveRow(from srcIndex: Int, to dstIndex: Int) {
+        assert(srcIndex >= 0 && srcIndex < self.rowCount, "Source index out of range.")
+        assert(dstIndex >= 0 && dstIndex < self.rowCount, "Destination index out of range.")
+        self.ensureUniqueness()
+        guard srcIndex != dstIndex else { return }
+        
+        self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: srcIndex, column: 0), count: self.columnCount), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: dstIndex, column: 0))
+    }
+    
+    mutating func moveRows(from srcRange: Range<Int>, to dstIndex: Int) {
+        assert(srcRange.lowerBound >= 0 && srcRange.lowerBound < self.rowCount, "Source range out of range.")
+        assert(srcRange.count + dstIndex <= self.rowCount, "Move not possible as it would overflow.")
+        self.ensureUniqueness()
+        guard srcRange.lowerBound != dstIndex else { return }
+        
+        let moveCount = srcRange.count * self.columnCount
+        self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: srcRange.lowerBound, column: 0), count: moveCount), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: dstIndex, column: 0))
+    }
+    
+    mutating func moveColumn(from srcIndex: Int, to dstIndex: Int) {
+        assert(srcIndex >= 0 && srcIndex < self.columnCount, "Source index out of range.")
+        assert(dstIndex >= 0 && dstIndex < self.columnCount, "Destination index out of range.")
+        self.ensureUniqueness()
+        guard srcIndex != dstIndex else { return }
+        
+        (0..<self.rowCount).forEach {
+            self.storage.moveElement(from: self.linearizedIndex(forRow: $0, column: srcIndex), to: self.linearizedIndex(forRow: $0, column: dstIndex))
+        }
+    }
+    
+    mutating func moveColumns(from srcRange: Range<Int>, to dstIndex: Int) {
+        assert(srcRange.lowerBound >= 0 && srcRange.lowerBound < self.columnCount, "Source range out of range.")
+        assert(srcRange.count + dstIndex <= self.columnCount, "Move not possible as it would overflow.")
+        self.ensureUniqueness()
+        guard srcRange.lowerBound != dstIndex else { return }
+        
+        (0..<self.columnCount).forEach {
+            self.storage.moveElements(fromRange: .init(startIndex: self.linearizedIndex(forRow: $0, column: srcRange.lowerBound), count: srcRange.count), intoCorrespondingRangeStartingAt: self.linearizedIndex(forRow: $0, column: dstIndex))
+        }
+    }
+}
+
+// MARK: - CustomDebugStringConvertible
+extension FixedDataTable: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        """
+        FixedDataTable<\(type(of: Element.self))>
+        - Size: \(self.rowCount) row\(self.rowCount > 1 ? "s" : "") × \(self.columnCount) column\(self.columnCount > 1 ? "s" : ""), totaling \(self.count) element\(self.count > 1 ? "s" : "").
+        - Rows:
+        \(self.rows.enumerated().map {
+            "\t\($0.offset): " + String(reflecting: $0.element)
+        }.joined(separator: "\n"))
+        """
+    }
+}
+
+// MARK: - CustomStringConvertible
+extension FixedDataTable: CustomStringConvertible {
+    public var description: String {
+        let maxSize = self.storage.toArray().map { String(reflecting: $0).count }.max() ?? 0
+        func overflow(_ item: Element) -> Int { maxSize - String(reflecting: item).count }
+        func itemCell(_ item: Element) -> String {
+            " \(String(repeating: " ", count: overflow(item)))\(item) "
+        }
+        let topLine = "┌\([String](repeating: String(repeating: "─", count: maxSize + 2), count: self.columnCount).joined(separator: "┬"))┐"
+        let interLine = "├\([String](repeating: String(repeating: "─", count: maxSize + 2), count: self.columnCount).joined(separator: "┼"))┤"
+        let bottomLine = "└\([String](repeating: String(repeating: "─", count: maxSize + 2), count: self.columnCount).joined(separator: "┴"))┘"
+        
+        let lines = [topLine] + [String](self.rows.map {
+            "│\($0.map(itemCell(_:)).joined(separator: "│"))│"
+        }.map { [$0] }.joined(separator: [interLine])) + [bottomLine]
+        
+        return lines.joined(separator: "\n")
     }
 }
