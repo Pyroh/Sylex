a9d285421b112399834a65fe005d20860cbbfc58
Pyroh <pyroh@icloud.com>
2020-09-29 17:50:15 +0200

    [ADD] Data to String method

---
+++ Sources/Sylex/Extensions/DataExtension.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pierre Tacchi on 29/09/20.
+//
+
+import Foundation
+
+public extension Data {
+    func string(encoding: String.Encoding = .utf8) -> String? {
+        String(data: self, encoding: encoding)
+    }
+}
