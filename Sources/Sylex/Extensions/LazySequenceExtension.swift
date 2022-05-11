31bb8e46a8569a6271a20aed8f52e4e95efe2686
Pyroh <pyroh@icloud.com>
2022-05-11 18:21:18 +0200

    [FIX] LazySequence filter return type

--- Sources/Sylex/Extensions/LazySequenceExtension.swift
+++ Sources/Sylex/Extensions/LazySequenceExtension.swift
@@ -8,107 +8,122 @@
 import Foundation
 
 public extension LazySequence {
-    @inlinable func filter<T: Equatable>(on key: KeyPath<Element, T>, equalTo value: T) -> [Element] {
+    @inlinable func filter<T: Equatable>(on key: KeyPath<Element, T>,
+                                         equalTo value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] == value }
     }
     
-    @inlinable func filter<T: Equatable>(on key: KeyPath<Element, T>, notEqualTo value: T) -> [Element] {
+    @inlinable func filter<T: Equatable>(on key: KeyPath<Element, T>,
+                                         notEqualTo value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] != value }
     }
     
-    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>, lessThan value: T) -> [Element] {
+    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>,
+                                          lessThan value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] < value }
     }
     
-    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>, greaterThan value: T) -> [Element] {
+    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>,
+                                          greaterThan value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] > value }
     }
     
-    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>, lessThanOrEqualTo value: T) -> [Element] {
+    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>,
+                                          lessThanOrEqualTo value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] <= value }
     }
     
-    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>, greaterThanOrEqualTo value: T) -> [Element] {
+    @inlinable func filter<T: Comparable>(on key: KeyPath<Element, T>,
+                                          greaterThanOrEqualTo value: T) -> LazyFilterSequence<Base> {
         filter { $0[keyPath: key] >= value }
     }
 }
 
 public extension LazySequence where Element: Equatable {
-    @inlinable func filter(equalTo value: Element) -> [Element] {
+    @inlinable func filter(equalTo value: Element) -> LazyFilterSequence<Base> {
         filter { $0 == value }
     }
     
-    @inlinable func filter(notEqualTo value: Element) -> [Element] {
+    @inlinable func filter(notEqualTo value: Element) -> LazyFilterSequence<Base> {
         filter { $0 == value }
     }
 }
 
 public extension LazySequence where Element: Comparable {
     
-    @inlinable func filter(lessThan value: Element) -> [Element] {
+    @inlinable func filter(lessThan value: Element) -> LazyFilterSequence<Base> {
         filter { $0 < value }
     }
     
-    @inlinable func filter(greaterThan value: Element) -> [Element] {
+    @inlinable func filter(greaterThan value: Element) -> LazyFilterSequence<Base> {
         filter { $0 > value }
     }
     
-    @inlinable func filter(lessThanOrEqualTo value: Element) -> [Element] {
+    @inlinable func filter(lessThanOrEqualTo value: Element) -> LazyFilterSequence<Base> {
         filter { $0 <= value }
     }
     
-    @inlinable func filter(greaterThanOrEqualTo value: Element) -> [Element] {
+    @inlinable func filter(greaterThanOrEqualTo value: Element) -> LazyFilterSequence<Base> {
         filter { $0 >= value }
     }
 }
 
 public extension LazySequence {
-    @inlinable func filter(on key: KeyPath<Element, Bool>) -> [Element] {
+    @inlinable func filter(on key: KeyPath<Element, Bool>) -> LazyFilterSequence<Base> {
         filter(on: key, equalTo: true)
     }
     
-    @inlinable func filter(onNot key: KeyPath<Element, Bool>) -> [Element] {
+    @inlinable func filter(onNot key: KeyPath<Element, Bool>) -> LazyFilterSequence<Base> {
         filter(on: key, equalTo: false)
     }
 }
 
 public extension LazySequence {
     
-    @inlinable func first<T: Equatable>(where key: KeyPath<Element, T>, equalTo value: T) -> Element? {
+    @inlinable func first<T: Equatable>(where key: KeyPath<Element, T>,
+                                        equalTo value: T) -> Element? {
         first { $0[keyPath: key] == value }
     }
     
-    @inlinable func first<T: Equatable>(where key: KeyPath<Element, T>, notEqualTo value: T) -> Element? {
+    @inlinable func first<T: Equatable>(where key: KeyPath<Element, T>,
+                                        notEqualTo value: T) -> Element? {
         first { $0[keyPath: key] != value }
     }
     
-    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>, lessThan value: T) -> Element? {
+    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>,
+                                         lessThan value: T) -> Element? {
         first { $0[keyPath: key] < value }
     }
     
-    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>, greaterThan value: T) -> Element? {
+    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>,
+                                         greaterThan value: T) -> Element? {
         first { $0[keyPath: key] > value }
     }
     
-    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>, lessThanOrEqualTo value: T) -> Element? {
+    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>,
+                                         lessThanOrEqualTo value: T) -> Element? {
         first { $0[keyPath: key] <= value }
     }
     
-    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>, greaterThanOrEqualTo value: T) -> Element? {
+    @inlinable func first<T: Comparable>(where key: KeyPath<Element, T>,
+                                         greaterThanOrEqualTo value: T) -> Element? {
         first { $0[keyPath: key] >= value }
     }
 }
 
 public extension LazySequence {
-    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>, contains substring: S) -> Element? {
+    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>,
+                                             contains substring: S) -> Element? {
         first { $0[keyPath: key].contains(substring) }
     }
     
-    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>, localizedCaseInsensitiveContains substring: S) -> Element? {
+    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>,
+                                             localizedCaseInsensitiveContains substring: S) -> Element? {
         first { $0[keyPath: key].localizedCaseInsensitiveContains(substring) }
     }
     
-    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>, localizedStandardContains substring: S) -> Element? {
+    @inlinable func first<S: StringProtocol>(where key: KeyPath<Element, String>,
+                                             localizedStandardContains substring: S) -> Element? {
         first { $0[keyPath: key].localizedStandardContains(substring) }
     }
 }
